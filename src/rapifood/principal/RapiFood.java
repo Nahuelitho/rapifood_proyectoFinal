894c425a4b78feb9e01d49af020dad766645cde5
Jonathan Salas <joni2210122@gmail.com>
2020-11-01 19:55:26 -0300

    Insertando clase MesaData

--- src/rapifood/principal/RapiFood.java
+++ src/rapifood/principal/RapiFood.java
@@ -11,19 +11,34 @@ public class RapiFood {
     
     public static void main(String[] args) {
         Conexion c = new Conexion();
-        Producto p = new Producto();
-        ProductoData pd = new ProductoData(c);
-        List<Producto>lprod=new ArrayList<>();
+        //Producto p = new Producto();
+        //ProductoData pd = new ProductoData(c);
+        //List<Producto>lprod=new ArrayList<>();
         //pd.DarDeBajaProducto(1);
         //lprod.addAll(pd.obtenerProductosDisponibles());
         //lprod.addAll(pd.obtenerProductos());
         //System.out.println(lprod);
         //pd.guardarProducto(p);
-        p=pd.buscarProducto(1);
-        p.setEstadoProducto(true);
-        p.setPrecio(90);
-        pd.actualizarProducto(p);
+        //p=pd.buscarProducto(1);
+        //p.setEstadoProducto(true);
+        //p.setPrecio(90);
+        //pd.actualizarProducto(p);
         //System.out.println(p);
+         //----------------------------prueba MesaData--------------------------//
+       //MesaData md = new MesaData(c);
+       //Mesa m= new Mesa();
+       //m.setCapacidadMaxima(3);
+       //m.setEstadoMesa(false);
+       //md.guardarMesa(m);
+       //md.buscarMesa(2);
+       //md.borrarMesa(1);
+       //List<Mesa>lismesa=new ArrayList<>();
+       //lismesa.addAll(md.obtenerMesasDisponibles());
+       //System.out.println(lismesa);
+       //lismesa.addAll(md.obtenerMesas());
+       //System.out.println(lismesa);
+       //md.darBajaMesa(3);
+       //md.darAltaMesa(3);
         
     }
 }
